7da7262ff41b08be41ece99e391527fc7a65f55a
Hà Trung Triển <trientrung102@gmail.com>
2023-10-14 10:23:45 +0700

    add WebApi

---
+++ Controllers/TestController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Shopee_Food.Controllers
+{
+    public class TestController : ApiController
+    {
+        //// GET api/<controller>
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/<controller>   : url to use => api/vs
+        public string Get()
+        {
+            return "Hi from web api controller";
+        }
+
+        // GET api/<controller>/5   : url to use => api/vs/5
+        public string Get(int id)
+        {
+            return (id + 1).ToString();
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
